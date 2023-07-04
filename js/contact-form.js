c87903f9198e142e7e7df9838c70f0286e36a33a
AnthonyChhor <anthonychhor27@gmail.com>
2023-07-04 09:49:26 -0500

    first commit

---
+++ js/contact-form.js
@@ -0,0 +1,9 @@
+// const blob = document.getElementById('mouse-blob');
+//
+// document.body.onpointermove = event => {
+//     const { clientX, clientY } = event;
+//     blob.animate({
+//         left: `${clientX}px`,
+//         top: `${clientY}px`
+//     }, { duration: 2000, fill: "forwards"});
+// }
